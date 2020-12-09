900020b50311822f0a66132165ff2374601c3991
GhendraNova <tristan.schneider2001@gmail.com>
2020-12-09 16:24:17 +0100

    ok

---
+++ exercice3/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $admin = false;
+    ?>
+    <h1>exercice 3</h1>
+
+    <?php
+
+    if($admin) {
+        ?>
+        <p>Bonjour Admin clique <a href="#">sur ce lien</a> pour gérer le site </p>
+        <?php
+
+    } else {
+        ?>
+        <p class="error">'Vous n etes pas admin'</p>
+
+        <?php
+}
+        ?>
+</body>
+</html>
