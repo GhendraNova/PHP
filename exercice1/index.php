99d6881fbe930aaf9e835a8d6614bddf73e2a446
GhendraNova <tristan.schneider2001@gmail.com>
2020-12-09 11:46:11 +0100

    fin de l'exo 1

---
+++ exercice1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>exercice 1</h1>
+    <?php
+
+    echo '<ul>
+         <li>Banane</li>
+         <li>Orange</li>
+         <li>Kiwi</li>
+         <li>Pomme</li>
+         <li>Poire</li>
+         </ul>';
+
+    ?>
+</body>
+</html>
