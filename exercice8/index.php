fa140117595360844bbf02cf7faaf495819b5e76
GhendraNova <tristan.schneider2001@gmail.com>
2020-12-10 15:09:39 +0100

    exo 8 bonus

--- exercice8/index.php
+++ exercice8/index.php
@@ -11,29 +11,39 @@
     $userInfos = [
         [
             'name' => 'Jean',
-            'birthdate' => '14/03/2000'
+            'birthdate' => '14/03/2000',
+            'country' => 'Argentine'
         ],
         [
             'name' => 'Marc',
-            'birthdate' => '15/08/1967'
+            'birthdate' => '15/08/1967',
+            'country' => 'Espagne'
         ],
         [
             'name' => 'Sophie',
-            'birthdate' => '12/11/1987'
+            'birthdate' => '12/11/1987',
+            'country' => NULL
         ],
         [
             'name' => 'Marie',
-            'birthdate' => '27/01/1956'
+            'birthdate' => '27/01/1956',
+            'country' => 'Russie'
         ],
         [
             'name' => 'Joseph',
-            'birthdate' => '31/12/2003'
+            'birthdate' => '31/12/2003',
+            'country' => 'Allemagne'
         ],
     ];
 
     foreach($userInfos as $info){
         echo '<h2>' . $info['name'] . '</h2>';
         echo '<p> Bonjour je m\'appelle ' . $info['name'] . ' et je suis né le ' . $info['birthdate'] . '</p>';
+        echo '<p> Cet utilisateur a visité : <ul> <li>' . $info['country'] . '</ul></li>';
+    }
+
+    if($userInfos['country']=['country' => NULL]){
+        echo '<p>Cet utilisateur n\'as visité aucun pays.</p>';
     }
 
     ?>
