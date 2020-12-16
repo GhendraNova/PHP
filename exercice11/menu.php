16e8db27ea641f506f9339d6ebdb1721cab9a84b
Ghendra <tristan.schneider2001@gmail.com>
2020-12-16 09:16:18 +0100

    ok

---
+++ exercice11/menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Index</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="shop.php">Boutique</a></li>
+        <li><a href="https://www.google.com">Google</a></li>
+    </ul>
+</nav>
